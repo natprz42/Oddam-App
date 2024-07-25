30aace9caa409eb5b7bb34472165b2ba3a339e52
natprz42 <natprz42@gmail.com>
2024-07-25 12:32:16 +0200

    Styling home page

--- oddam-app/src/components/HomeHeader.js
+++ oddam-app/src/components/HomeHeader.js
@@ -1,49 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Decoration from '../assets/Decoration.png'
-
+import HeroImage from '../assets/HeroImage.png'
 const HomeHeader = () => {
     return (
-        <div>
+        <div className="main">
             <div className="login">
-                <button>
+                <button className="btn1">
                     <Link to="/login">Zaloguj</Link>
                 </button>
-                <button>
+                <button className="btn1">
                     <Link to="/registration">Załóż konto</Link>
                 </button>
             </div>
 
             <div className='navBar'>
-                <div className='start'>
-                    <Link to="/">Start</Link>
-                </div>
-                <div className='description'>
-                    <Link to="/description">O co chodzi?</Link>
-                </div>
-                <div className='about'>
-                    <Link to="/about">O nas</Link>
-                </div>
-                <div className='foundation'>
-                    <Link to="/foundation">Fundacja i organizacje</Link>
-                </div>
-                <div className='contact'>
-                    <Link to="contact">Kontakt</Link>
-                </div>
+                <ul>
+                    <li>
+                        <Link to="/">Start</Link>
+                    </li>
+                    <li>
+                        <Link to="/description">O co chodzi?</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">O nas</Link>
+                    </li>
+                    <li>
+                        <Link to="/foundation">Fundacja i organizacje</Link>
+                    </li>
+                    <li>
+                        <Link to="contact">Kontakt</Link>
+                    </li>
+                </ul>
             </div>
+            <img className="heroImage" src={HeroImage} alt={"decoration"}/>
+            <div className="homePage">
+                <div className="slogan">
+                    <h1>Zacznij pomagać!</h1>
+                    <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                </div>
+                <img className="decoration" src={Decoration} alt={"decoration"}/>
 
-            <div className="slogan">
-                <h1>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
-            </div>
-            <img src={Decoration} alt={"decoration"}/>
-
-            <div className="buttons_1">
-                <button>
-                    <Link to="/login">ODDAJ RZECZY</Link>
-                </button>
-                <button>
-                    <Link to="/login">ZORGANIZUJ ZBIÓRKĘ</Link>
-                </button>
+                <div className="buttonSection">
+                    <button className="btn2">
+                        <Link to="/login">ODDAJ RZECZY</Link>
+                    </button>
+                    <button className="btn2">
+                        <Link to="/login">ZORGANIZUJ ZBIÓRKĘ</Link>
+                    </button>
+                </div>
             </div>
         </div>
     );
